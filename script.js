c66cba774b32be0af6e6a674afba282868866844
YikNow <danyavodolaz@gmail.com>
2025-12-02 16:11:58 -0700

    1

--- script.js
+++ script.js
@@ -2,11 +2,23 @@ let menu = document.querySelector(".fixed-menu");
 menu.addEventListener("click", function (event){
    let clickTarget = event.target;
    let activeBtn = document.querySelector(".active");
-
    if(clickTarget.classList.contains("nav-link")){
        clickTarget.classList.add("active");
        activeBtn.classList.remove("active");
+       let mobile = document.querySelector(".mobile-menu");
+       if (!mobile.classList.contains("hide")){
+           mobile.classList.add("hide");
+       }
+       let body = document.querySelector("body");
+       let mobileMenu = document.querySelector(".mobile-menu");
+       if  (!body.classList.contains("off-scroll") && !mobileMenu.classList.contains("hide")){
+           body.classList.add("off-scroll");
+       }
+       else{
+           body.classList.remove("off-scroll");
+       }
    };
+
 });
 let classLink = '.main-link';
 window.onscroll = function(){
